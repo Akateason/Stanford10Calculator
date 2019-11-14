04eb33e980b74c236867e62851c29b4dfed261d4
谢天宸 <xietianchen@shimo.im>
2019-11-14 21:23:16 +0800

    Initial Commit

---
+++ Stanford10Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stanford10Calculator
+//
+//  Created by teason23 on 2019/11/14.
+//  Copyright © 2019 teason23. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
