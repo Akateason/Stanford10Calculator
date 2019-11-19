2648309ed51a7874f0ed5a4cc3a9cb14cab8052c
谢天宸 <xietianchen@shimo.im>
2019-11-19 17:35:17 +0800

    start todo

---
+++ Stanford10Calculator/takeNote/NoteForClass3.swift
@@ -0,0 +1,17 @@
+//
+//  NoteForClass3.swift
+//  Stanford10Calculator
+//
+//  Created by teason23 on 2019/11/19.
+//  Copyright © 2019 teason23. All rights reserved.
+//
+
+import UIKit
+
+class NoteForClass3: NSObject {
+
+    
+    todo
+    
+    
+}
